32a381b095cbb69467e88c911c51f6ef49c7c016
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-01-18 18:47:39 +0100

    First commit

---
+++ components/page-intro/index.tsx
@@ -0,0 +1,16 @@
+const PageIntro = () => {
+  return (
+    <section className="page-intro">  
+          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+            <div className="container">
+              <div className="page-intro__slide__content">
+                <h2>Trusted by customers, partners and the industry</h2>
+                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+              </div>
+            </div>
+          </div>
+    </section>
+  )
+};
+
+export default PageIntro
---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+// types
+import type { AppProps } from 'next/app';
+
+// global styles
+// import 'swiper/swiper.scss';
+// import 'rc-slider/assets/index.css';
+// import 'react-rater/lib/react-rater.css';
+import '../assets/css/styles.scss';
+
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Fragment>
+            <Component {...pageProps} />
+        </Fragment>
+    );
+};
+
+export default MyApp;
