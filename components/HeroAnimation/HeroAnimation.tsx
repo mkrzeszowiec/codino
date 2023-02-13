cd2925cd3b00898dc23848f53bbdcd1f35240e29
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-02-13 12:16:33 +0100

    Render hero animation only if visible

--- components/HeroAnimation/HeroAnimation.tsx
+++ components/HeroAnimation/HeroAnimation.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
+import RenderIfVisible from 'react-render-if-visible';
 
 const HeroAnimation = () => {
 
 	return (
-		<>
+		<RenderIfVisible>
 			<Head>
 				<script type="text/javascript" defer src="../star-animation/vendors.js"></script>
 				<script type="text/javascript" defer src="../star-animation/index.js"></script>
@@ -12,7 +13,7 @@ const HeroAnimation = () => {
 			<div id="hero-animation" />
 			<div className="background-hero-animation" />
 			<div className="overlay-hero-animation" />
-		</>
+		</RenderIfVisible>
 	);
 };
 
