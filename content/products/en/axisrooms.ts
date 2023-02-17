9eefb3cbb2e2370a85edce30ae9bf593b7514287
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-02-17 22:52:27 +0100

    Add texts to the axisroomsx

--- content/products/en/axisrooms.ts
+++ content/products/en/axisrooms.ts
@@ -1,29 +1,48 @@
 import { Product } from 'types/common';
 
 const AXISROOMS: Product = {
-	title: 'Axisrooms - Property management system',
-	description: 'Software for managing room availability and rates in the tourism industry.',
+	title: 'Axisrooms CRS',
+	shortDescription:
+		'Axisrooms Central Reservation System is a powerful system for managing hotel and apartment chains operating in India.',
+	description:
+		'Axisrooms Central Reservation System is a powerful system for managing hotel and apartment chains operating in India. The software allows you to configure all hotel parameters starting from basic data like number and types of available rooms, price lists, reservation and cancellation policies, meal plans, taxes, add-on packages, promotions. In addition, the system has its own reservation system that allows employees to add reservations on the spot and synchronize with external reservation providers such as Booking.com',
 	imageSource: '/images/case-study/axisrooms/cover.png',
 	slug: 'axisrooms',
 	color: '#ea1f61',
 	date: '-',
 	countries: ['IND'],
 	scopeOfTechnology: '-',
+	scopeOfWork: {
+		description: `The goal of our team was to prepare the entire backend and frontend from scratch according to the documentation provided to us. Working on the project was 3 years of development and constant communication with the client's analysts and testers to deliver the highest quality product`,
+		points: [
+			{
+				index: 1,
+				description:
+					'detailed understanding of the hospitality domain and complete design of the backend and frontend architecture'
+			},
+			{
+				index: 2,
+				description:
+					'Design and implementation of a frontend to easily and transparently handle the complex aspects of hotel and reservation management'
+			},
+			{
+				index: 3,
+				description:
+				'Design and implementation of a scalable backend allowing to handle thousands of hotels and their reservations'
+			},
+			{
+				index: 4,
+				description:
+					'Design a heuristic for handling reservations that allows for optimal distribution of hotel guests by available rooms'
+			},
+			{
+				index: 5,
+				description:
+				'Integrations with customer systems providing booking information from external systems'
+			}
+		]
+	},
 	industry: 'tourism, crm',
-	solutions: [
-		{
-			title: 'Solution 1',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/axisrooms/1.png'
-		},
-		{
-			title: 'Solution 2',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/axisrooms/2.png'
-		}
-	]
 };
 
 export default AXISROOMS;
--- content/products/pl/axisrooms.ts
+++ content/products/pl/axisrooms.ts
@@ -1,29 +1,49 @@
 import { Product } from 'types/common';
 
 const AXISROOMS: Product = {
-	title: 'Axisrooms - System zarządzania nieruchomościami',
-	description: 'Oprogramowanie do zarządzania dostępnością i cenami pokoi w branży turystycznej.',
+	title: 'Axisrooms CRS',
+	shortDescription:
+		'Axisrooms Central Reservation System to rozbudowany system do zarządzania sieciami hoteli i apartamentów działający w Indiach.',
+	description:
+		'Axisrooms Central Reservation System to rozbudowany system do zarządzania sieciami hoteli i apartamentów działający w Indiach. Oprogramowanie pozwala na konfigurację wszystkich parametrów hotelu zaczynając od podstawowych danych jak liczba i rodzaje dostępnych pokoi, cenniki, polityki rezerwacji i anulowania, planów żywieniowych, podatków, pakietów dodatków, promocji. Dodatkowo system posiada własny system rezerwacji umożliwiający dodawanie rezerwacji na miejscu przez pracowników oraz synchronizację z zewnętrznymi dostawcami rezerwacji np Booking.com',
 	imageSource: '/images/case-study/axisrooms/cover.png',
 	slug: 'axisrooms',
 	color: '#ea1f61',
 	date: '-',
 	countries: ['IND'],
 	scopeOfTechnology: '-',
+	scopeOfWork: {
+		description:
+			'Celem naszego zespołu było przygotowanie całego backendu i frontendu od początku wg dostarczonej nam dokumentacji. Praca nad projektem to 3 lata developmentu i nieustannej komunikacji z analitykami i testerami klienta w celu dostarczenia najwyższej jakości produktu',
+		points: [
+			{
+				index: 1,
+				description:
+					'Szczegółowe zapoznanie się z domeną hotelarską oraz całkowite zaprojektowanie architektury backendowej i frontendowej'
+			},
+			{
+				index: 2,
+				description:
+					'Zaprojektowanie i implementacja frontendu pozwalającego w łatwy i przejrzysty sposób na obsługę skomplikowanych aspektów związanych z zarządzanie hotelem i rezerwacjami'
+			},
+			{
+				index: 3,
+				description:
+					'Zaprojektowanie i implementacja skalowalnego backendu pozwalającego obsługę tysięcy hoteli i ich rezerwacji'
+			},
+			{
+				index: 4,
+				description:
+					'Zaprojektowanie heurystyki do obsługi rezerwacji która pozwala na optymalne rozmieszczenie gości hotelowych po dostępnych pokojach'
+			},
+			{
+				index: 5,
+				description:
+					'Integracje z systemami klienta dostarczającymi informacje o rezerwacjach z zewnętrznych systemów'
+			}
+		]
+	},
 	industry: 'turystyka, crm',
-	solutions: [
-		{
-			title: 'Rozwiązanie 1',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/axisrooms/1.png'
-		},
-		{
-			title: 'Rozwiązanie 2',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/axisrooms/2.png'
-		}
-	]
 };
 
 export default AXISROOMS;
