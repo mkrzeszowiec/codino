97ed5719031dc1f05b55933656580d92e86213a5
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-02-20 10:43:34 +0100

    Update texts in services pages

--- content/services/pl/currentPositions.ts
+++ content/services/pl/currentPositions.ts
@@ -4,7 +4,7 @@ export const SERVICES_PL: Position[] = [
 	{
 		title: 'Backend',
 		description:
-			'Odpowiedni dobór technologii do projektu jest niezbędny. Decyzja o technologii backendu Twojej aplikacji lub strony internetowej będzie miała wpływ na Ciebie przez długi czas, dlatego ważne jest, aby dobrze to przemyśleć. Backend postrzegamy jako rdzeń działania Twojej firmy, miejsce, w którym odbywają się najważniejsze procesy i generowana jest wartość. \n\n' +
+			'Odpowiedni dobór technologii do projektu jest niezbędny. Decyzja o technologii backendu Twojej aplikacji lub strony internetowej będzie miała wpływ na Twój projekt przez długi czas, dlatego ważne jest, aby dobrze to przemyśleć. Backend postrzegamy jako rdzeń działania Twojej firmy, miejsce, w którym odbywają się najważniejsze procesy i generowana jest wartość.  \n\n' +
 			'Zapewniamy kompleksową realizację projektu, poczynając od zaprojektowania systemu, poprzez opracowanie dokumentacji, napisanie kodu i wdrożenie gotowego produktu. Możemy współpracować jako niezależni specjaliści lub jako część większego zespołu w ramach outsourcingu.',
 		shortDescription: '',
 		slug: 'backend',
@@ -28,7 +28,7 @@ export const SERVICES_PL: Position[] = [
 	{
 		title: 'Frontend',
 		description:
-			'Rozwijanie frontendu jest istotne dla utrzymania satysfakcji użytkownika, ponieważ decyduje o tym, co widzą i jak wchodzą w interakcje z aplikacją. Aby zbudować atrakcyjną i funkcjonalną aplikację, niezbędne jest zadbanie o jakość frontendu.\n\n' +
+			'Rozwijanie frontendu jest istotne dla utrzymania satysfakcji użytkownika, ponieważ decyduje o tym, co widzą i jak wchodzą w interakcje z aplikacją. Aby zbudować atrakcyjną i funkcjonalną aplikację, niezbędne jest zadbanie o wysoką jakość frontendu. \n\n' +
 			'Dobieramy frameworki i narzędzia, aby spełniać wymagania biznesowe i współczesne standardy. ' +
 			'Korzystając z nowoczesnych technologii, zapewniamy, że Twoja aplikacja będzie szybka, łatwa w użyciu i przede wszystkim piękna. Potrafimy być niezależnym zespołem lub częścią zespołu developerskiego.',
 		shortDescription: '',
