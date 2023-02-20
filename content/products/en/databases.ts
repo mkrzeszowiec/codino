178933976e7ff1900ca6d3e40db0a0444992fd6d
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-02-20 14:12:25 +0100

    Update content in 'databases' project

--- content/products/en/databases.ts
+++ content/products/en/databases.ts
@@ -1,29 +1,40 @@
 import { Product } from 'types/common';
 
 const DATABASES: Product = {
-	title: 'Databases - Database tool development and optimization.',
-	description:
+	title: 'Databases - Database tool development and optimization',
+	shortDescription:
 		'Cloud client for non-relational databases. An innovative approach to managing non-relational databases.',
+	description: `Our team participated in the development of a web tool for managing non-relational databases. The database client is run in the cloud, so it does not burden the user's machine and has very fast access to data. The tool allows users to handle and perform complex operations on non-relational databases.`,
+	expandedDescription: {
+		description: 'Some of the options available in the cloud version of the database client: ',
+		points: [
+			'database viewer',
+			'document editor in different modes (json, table, tree)',
+			'database/container management',
+			'data visualization',
+			'document structure analysis',
+			'query performance analysis',
+			'task automation (container copying, mass document editing)',
+			'import/export from file and sql database',
+			'analyzer for indexes',
+			'clickable query builder'
+		]
+	},
 	imageSource: '/images/case-study/databases/cover.png',
 	slug: 'databases',
 	color: '#ea801f',
 	date: '-',
-	scopeOfTechnology: 'databases',
-	industry: '-',
-	solutions: [
-		{
-			title: 'Solution 1',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/databases/1.png'
-		},
-		{
-			title: 'Solution 2',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/databases/2.png'
-		}
-	]
+	scopeOfWork: {
+		description:
+			'Our team was responsible for the development of the backend and frontend application. The entire system is hosted and run in the cloud and the client can run the database client in a web browser. ',
+		points: [
+			{ index: 1, description: 'Java/Spring backend development' },
+			{ index: 2, description: 'making a frontend application based on React framework' },
+			{ index: 3, description: 'optimization of database client performance' }
+		]
+	},
+	technologies: ['java', 'spring', 'react'],
+	countries: ['']
 };
 
 export default DATABASES;
--- content/products/pl/databases.ts
+++ content/products/pl/databases.ts
@@ -1,29 +1,41 @@
 import { Product } from 'types/common';
 
 const DATABASES: Product = {
-	title: 'Bazy danych - Tworzenie narzędzi bazodanowych i optymalizacja.',
-	description:
+	title: 'Bazy danych - Tworzenie narzędzi bazodanowych i optymalizacja',
+	shortDescription:
 		'Chmurowy klient do nieralacynych baz danych. Innowacyjne podejście do zarządzania nierelacyjymi bazami danych.',
+	description:
+		'Nasz zespół brał udział w tworzeniu narzędzia webowego do zarządzania nierelacyjnymi bazami danych. Klient bazy danych jest uruchamiany w chmurze, dzięki czemu nie obciąża maszyny użytkownika i ma bardzo szybki dostęp do danych. Narzędzie pozwala na obsługę i wykonywanie skomplikowanych operacji na nierelacyjnych bazach.',
+	expandedDescription: {
+		description: 'Niektóre z opcji dostępnych w chmurowej wersji klienta bazy danych: ',
+		points: [
+			'przeglądarka bazy',
+			'edytor dokumentów w różnych trybach (json, tabelka, drzewko)',
+			'zarządzanie bazami/kontenerami',
+			'wizualizacja danych',
+			'analiza struktury dokumentów',
+			'analiza wydajności zapytań',
+			'automatyzacja zadań (kopiowanie kontenerów, masowa edycja dokumentów)',
+			'import/eksport z pliku i bazy sql',
+			'analizator do indeksów',
+			'klikany kreator do zapytań'
+		]
+	},
 	imageSource: '/images/case-study/databases/cover.png',
 	slug: 'databases',
 	color: '#ea801f',
 	date: '-',
-	scopeOfTechnology: 'bazy danych',
-	industry: '-',
-	solutions: [
-		{
-			title: 'Rozwiązanie 1',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/databases/1.png'
-		},
-		{
-			title: 'Rozwiązanie 2',
-			description:
-				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium, aliquam cupiditate enim et exercitationem expedita fugiat illo ipsum magni nulla omnis possimus provident quibusdam quisquam saepe sed sit voluptatibus!',
-			imageSrc: '/images/case-study/databases/2.png'
-		}
-	]
+	scopeOfWork: {
+		description:
+			'Nasz zespół odpowiadał za development backendu oraz aplikacji frontendowej. Cały system jest hostowany i uruchamiany w chmurze a klient może uruchamiać klienta bazy danych w przeglądarce internetowej. ',
+		points: [
+			{ index: 1, description: 'wykonanie backendu w technologii Java/Spring' },
+			{ index: 2, description: 'wykonanie aplikacji frontendowej na bazie frameworka React' },
+			{ index: 3, description: 'optymalizacja działania klienta bazy danych' }
+		]
+	},
+	technologies: ['java', 'spring', 'react'],
+	countries: ['']
 };
 
 export default DATABASES;
