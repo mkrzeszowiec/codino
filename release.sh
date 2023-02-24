51013507a5a83199f246c9173b1d42578ecd1327
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-02-24 10:04:17 +0100

    Fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo  "******* fetch new version *******"
+git fetch
 git reset --hard origin/main
+
+echo  "******* set correct node version *******"
+~/.nvm/nvm.sh use 16.10.0
 
 echo "******* install dependencies *******"
 npm install
