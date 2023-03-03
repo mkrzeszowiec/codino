243d130c99aee9e5a1c4814238aa14ff39e595b5
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-03-03 09:20:11 +0100

    Revert "Fix 404 issue using next.config"
    This reverts commit 05b3825be2d16f68b6b50e4202c8ddd3528db120.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 const path = require('path');
 const { i18n } = require('./next-i18next.config');
-const { nextI18NextRewrites } = require('next-i18next/rewrites');
-
-const localeSubpaths = {
-	pl: 'pl',
-	en: 'en'
-};
 
 const withPWA = require('next-pwa')({
 	dest: 'public',
@@ -14,10 +8,6 @@ const withPWA = require('next-pwa')({
 });
 
 const nextConfig = withPWA({
-	rewrites: async () => nextI18NextRewrites(localeSubpaths),
-	publicRuntimeConfig: {
-		localeSubpaths
-	},
 	webpack: (config, { isServer }) => {
 		if (!isServer) {
 			config.resolve.fallback.fs = false;
