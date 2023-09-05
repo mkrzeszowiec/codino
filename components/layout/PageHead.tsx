46fb1956baa9cbbfd39501c09c8470786c259715
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-09-05 13:14:42 +0200

    Add google-site-verification meta tag

--- components/layout/PageHead.tsx
+++ components/layout/PageHead.tsx
@@ -15,6 +15,7 @@ const PageHead: React.FC<HeadProps> = ({ title }) => {
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta name="description" content={t('common.metadata.description')} />
 			<meta name="theme-color" content={colors.primary} />
+			<meta name="google-site-verification" content="WQtsQTJg0DniKhERMOBXPOVamvWLjlgQdeHOBAtVHcY" />
 			<link rel="manifest" href="/manifest.json" />
 			<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 			<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
