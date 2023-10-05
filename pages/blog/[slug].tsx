dd42fe53dc9f2638b57d995ce766f89d0f42948c
mkrzeszowiec <marek.krzeszowiec@codino.pl>
2023-10-05 15:51:02 +0200

    Update single article page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -45,19 +45,39 @@ export const getStaticProps: GetStaticProps = async context => {
 
 const ArticlePage = ({ article }: Props) => {
 	if (!article) return null;
+	const { title, author, mainPhotoUrl, content } = article;
 
 	return (
 		<MainLayout title={getPageTitle(article.title)}>
 			<article className="article">
 				<div className="container">
-					<Breadcrumbs className="article__breadcrumbs" currentPage={article.title} type={'blog'} />
+					<Breadcrumbs className="article__breadcrumbs" currentPage={title} type={'blog'} />
 
-					<div className="article__coverWrapper">
-						<Image className="article__cover" src={article.mainPhotoUrl} width={1500} height={1500} alt="" />
+					<h1 className="article__title">{title}</h1>
+
+					<div className="article__bottomInfoWrapper">
+						{author.imageUrl && (
+							<div className="article__authorImageWrapper">
+								<Image className="article__authorImage" src={author.imageUrl} alt="" width={36} height={36} />
+							</div>
+						)}
+
+						<div className="article__otherInfo">
+							<p className="article__authorName">{author.name}</p>
+
+							<div className="article__timeWrapper">
+								<p className="article__date">Jun 14</p>
+								<span className="article__divider" />
+								<p className="article__readTime">8 min read</p>
+							</div>
+						</div>
 					</div>
 
-					<h1 className="article__title">{article.title}</h1>
-					<div className="article__content">{article.content}</div>
+					<div className="article__coverWrapper">
+						<Image className="article__cover" src={mainPhotoUrl} width={1500} height={1500} alt="" />
+					</div>
+
+					<div className="article__content">{content}</div>
 				</div>
 			</article>
 		</MainLayout>
